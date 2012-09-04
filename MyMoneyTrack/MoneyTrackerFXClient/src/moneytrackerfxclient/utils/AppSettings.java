a7c886fc6ca41d2fa45710a666455927ed15ded7
teddypickerfromul <teddypickerfromul@gmail.com>
2012-09-04 10:01:02 +0400

    Дальше

--- MyMoneyTrack/MoneyTrackerFXClient/src/moneytrackerfxclient/utils/AppSettings.java
+++ MyMoneyTrack/MoneyTrackerFXClient/src/moneytrackerfxclient/utils/AppSettings.java
@@ -1,8 +1,17 @@
 package moneytrackerfxclient.utils;
 
+import java.io.File;
+import java.io.IOException;
+import org.ini4j.InvalidFileFormatException;
+import org.ini4j.Wini;
+
 public class AppSettings {
 
     private static volatile AppSettings instance;
+    private Wini inifile;
+    
+    private static final String default_file_name = "appsettings.ini";
+    private static final String default_credentials_section_name = "credentials";
   
     public static AppSettings getInstance() {
         AppSettings localInstance = instance;
@@ -16,5 +25,18 @@ public class AppSettings {
         }
         return localInstance;
     }
+
+    public static String getDefault_file_name() {
+        return default_file_name;
+    }
+
+    public static String getDefault_credentials_section_name() {
+        return default_credentials_section_name;
+    }
    
+    public static void createEmptyConfigGile() throws IOException{
+        File f = new File(default_file_name);
+        f.createNewFile();
+        
+    }
 }
